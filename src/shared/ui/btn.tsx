868b410a86e0e437a7d82fe4b7517e60350faec3
emonsvetov <monsvetov@gmail.com>
2026-05-13 01:27:56 +0500

    update Button component styles for improved hover and active effects

--- src/shared/ui/btn.tsx
+++ src/shared/ui/btn.tsx
@@ -13,7 +13,7 @@ const Button = ({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`py-[8px] xl:py-[10px] 2xl:py-[12px] 3xl:py-[16px] px-[16px] xl:px-[20px] 2xl:px-[24px] 3xl:px-[30px] text-[15px] xl:text-[15px] 2xl:text-[18px] 3xl:text-[24px] font-semibold tracking-widest transition-opacity disabled:opacity-30 ${className}`}
+      className={`py-[8px] xl:py-[10px] 2xl:py-[12px] 3xl:py-[16px] px-[16px] xl:px-[20px] 2xl:px-[24px] 3xl:px-[30px] text-[15px] xl:text-[15px] 2xl:text-[18px] 3xl:text-[24px] font-semibold tracking-widest cursor-pointer transition-all duration-200 hover:brightness-110 hover:scale-[1.03] active:scale-95 active:brightness-90 disabled:opacity-30 disabled:cursor-not-allowed disabled:hover:scale-100 disabled:hover:brightness-100 ${className}`}
     >
       {title}
     </button>
